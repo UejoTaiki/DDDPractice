6432e530e9c6b9b340b5b635899fb04f016803ae
TaikiUejo <tiki.ur.tiki@icloud.com>
2022-06-26 00:00:03 +0900

    view側のデータバインディングの設定

--- DDDPractice/View/WeatherLatestView.cs
+++ DDDPractice/View/WeatherLatestView.cs
@@ -13,17 +13,26 @@ namespace DDDPractice
 {
     public partial class WeatherLatestView : Form
     {
-        private WeatherLatestViewModel viewModel = new WeatherLatestViewModel();
+        private WeatherLatestViewModel _viewModel = new WeatherLatestViewModel();
 
         public WeatherLatestView()
         {
             InitializeComponent();
             StartPosition = FormStartPosition.CenterScreen;
+
+            this.AreaIdTextBox.DataBindings.Add(
+                "Text", _viewModel, nameof(_viewModel.AreaIdText));
+            this.DataDateLabel.DataBindings.Add(
+              "Text", _viewModel, nameof(_viewModel.DataDateText));
+            this.ConditionLabel.DataBindings.Add(
+              "Text", _viewModel, nameof(_viewModel.ConditionText));
+            this.TemperatureLabel.DataBindings.Add(
+              "Text", _viewModel, nameof(_viewModel.TemperatureText));
         }
 
         private void LatestButton_Click(object sender, EventArgs e)
         {
-            
+            _viewModel.search();
         }
     }
 }
