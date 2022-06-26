47ed9c5aed3c9a19da76b5f09a33cd2242119846
TaikiUejo <tiki.ur.tiki@icloud.com>
2022-06-26 11:16:11 +0900

    Moqの導入

--- DDDTest.Tests/WeatherLatestViewModelTest.cs
+++ DDDTest.Tests/WeatherLatestViewModelTest.cs
@@ -2,6 +2,7 @@
 using DDD.Domain.Repositories;
 using DDDPractice.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 
 namespace DDDTest.Tests
@@ -12,7 +13,11 @@ namespace DDDTest.Tests
         [TestMethod]
         public void シナリオ()
         {
-            var viewModel = new WeatherLatestViewModel(new WeatherMock());
+            var weatherMock = new Mock<IWeatherRepository>();
+            weatherMock.Setup(x => x.GetLatest(1))
+                .Returns(new WeatherEntity(1, Convert.ToDateTime("2022/06/23 13:15:44"), 2, 12.3f));
+            var viewModel = new WeatherLatestViewModel(weatherMock.Object);
+
             Assert.AreEqual("", viewModel.AreaIdText);
             Assert.AreEqual("", viewModel.DataDateText);
             Assert.AreEqual("", viewModel.ConditionText);
@@ -24,16 +29,24 @@ namespace DDDTest.Tests
             Assert.AreEqual("2022/06/23 13:15:44", viewModel.DataDateText);
             Assert.AreEqual("曇り", viewModel.ConditionText);
             Assert.AreEqual("12.30℃", viewModel.TemperatureText);
+
+            weatherMock.Setup(x => x.GetLatest(2)).
+                Returns(new WeatherEntity(2, Convert.ToDateTime("2022/06/23 13:15:44"), 2, 12.3f));
+
+            viewModel.AreaIdText = "2";
+            viewModel.search();
+            Assert.AreEqual("2", viewModel.AreaIdText);
+            Assert.AreEqual("2022/06/23 13:15:44", viewModel.DataDateText);
+            Assert.AreEqual("曇り", viewModel.ConditionText);
+            Assert.AreEqual("12.30℃", viewModel.TemperatureText);
         }
     }
 
-    internal class WeatherMock : IWeatherRepository
-    {
-        public WeatherEntity GetLatest(int areaId)
-        {
-            return new WeatherEntity(1, Convert.ToDateTime("2022/06/23 13:15:44"), 2, 12.3f);
-        }
-
-
-    }
+    //internal class WeatherMock : IWeatherRepository
+    //{
+    //    public WeatherEntity GetLatest(int areaId)
+    //    {
+    //        return new WeatherEntity(1, Convert.ToDateTime("2022/06/23 13:15:44"), 2, 12.3f);
+    //    }
+    //}
 }
